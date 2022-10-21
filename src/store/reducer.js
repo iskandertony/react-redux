2c0190bb968080f47effda98e27c46b6d15911eb
Mansur Mabo <maboiskanderr@gmail.com>
2022-10-21 17:07:56 +0600

    begin redux

---
+++ src/store/reducer.js
@@ -0,0 +1,18 @@
+import * as actions from './actionTypes';
+
+const initialState = {
+    tasks: []
+}
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case actions.TASK_ADD:
+      return {
+        ...state, 
+        tasks: [...state.tasks, action.payload]
+      };
+
+    default:
+      return state;
+  }
+}
